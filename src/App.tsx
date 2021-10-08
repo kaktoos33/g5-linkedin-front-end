da117f7e47178c6270b7010c0c9e724cbef51b42
Mehdi Anoosheh <med.anooshe@gmail.com>
2021-10-08 15:38:39 +0330

    react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Switch, Route } from "react-router-dom";
+import Login from './components/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+interface AppProps {
+  
 }
+ 
+interface AppState {
+  token:boolean;
+  
+}
+ 
+class App extends React.Component<AppProps, AppState> {
+ 
+  
+  render() { 
+    return ( 
+      <div className="container mt-3">
+        <Switch>
+          
+          <Route exact path="/login" component={Login} />
+            {/* <Route exact path={["/", "/home"]} component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/profile" component={Profile} /> */}
+            
+          </Switch>
+        </div>
 
+     );
+  }
+}
+ 
 export default App;
