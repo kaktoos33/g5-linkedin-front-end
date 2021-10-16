0a92d771b2db71f3b268ab326d728583fafeac0d
Mehdi Anoosheh <med.anooshe@gmail.com>
2021-10-16 21:21:46 +0330

    Install fontAwesom for icons

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,22 +1,13 @@
-import React from 'react'
+import React from "react";
 
-interface LoginProps {
-    
-}
- 
-interface LoginState {
-    
-}
- 
+interface LoginProps {}
+
+interface LoginState {}
+
 class Login extends React.Component<LoginProps, LoginState> {
-    
-    render() { 
-        return ( 
-            <div >
-            Login
-          </div>
-         );
-    }
+  render() {
+    return <div>Login</div>;
+  }
 }
- 
+
 export default Login;
