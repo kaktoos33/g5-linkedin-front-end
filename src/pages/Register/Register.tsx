2f8f77fe18ecd50c43f57b22879934c51f197873
amirbahador <amirbahador.pv@gmail.com>
2021-10-22 18:34:50 -0400

    fix ts bugs and Comment navbar

---
+++ src/pages/Register/Register.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FunctionComponent } from 'react';
+import {Header} from "./components/header/Header";
+
+
+export const Register : FunctionComponent = () => {
+    return (
+
+        <div className="Login min-h-screen bg-gray-50 flex flex-col justify-center">
+            <Header />
+            {/*<MyForm />*/}
+
+        </div>
+    );
+};
---
+++ src/pages/Register/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import './header.style.scss';
+
+export const Header = () => {
+    return (
+        <div className="max-w-md w-full mx-auto">
+            <h1 className="register-header-name">
+                ثبت نام
+            </h1>
+        </div>
+    );
+};
