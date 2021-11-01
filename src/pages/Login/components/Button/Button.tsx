fc0533b1b62b62a2c00085123084fdcb5c2552a1
amirbahador <amirbahador.pv@gmail.com>
2021-11-01 17:43:48 -0400

    change style in order to tell diffrence

--- src/pages/Login/components/Button/Button.tsx
+++ src/pages/Login/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 import { ButtonHTMLAttributes, FC } from "react";
-
+import "./style.scss"
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export const Button: FC<ButtonProps> = (props: ButtonProps) => {
@@ -12,10 +12,10 @@ export const Button: FC<ButtonProps> = (props: ButtonProps) => {
 
 export const PrimaryButton: FC<ButtonProps> = (props: ButtonProps) => {
   return (
-    <div className="flex items-center pb-2">
+    <div className="flex items-center pb-2 register-button">
       <button
         {...props}
-        className="w-full h-12 font-bold uppercase transition-all duration-150 ease-linear bg-transparent bg-blue-600 border border-blue-300 border-solid rounded-full outline-none :text-white w-fullh-12 hover:bg-blue-500 hover:text-white active:bg-blue-600 focus:outline-none"
+        className="button-primary w-full h-12 font-bold uppercase transition-all duration-150 ease-linear bg-transparent bg-blue-600 border border-blue-300 border-solid rounded-full outline-none :text-white w-fullh-12 hover:bg-blue-500 hover:text-white active:bg-blue-600 focus:outline-none"
         type="submit"
       >
         ورود
@@ -26,10 +26,10 @@ export const PrimaryButton: FC<ButtonProps> = (props: ButtonProps) => {
 
 export const SecondaryButton: FC<ButtonProps> = (props: ButtonProps) => {
   return (
-    <div className="flex items-center pb-2">
+    <div className="flex items-center pb-2 register-button">
       <button
         {...props}
-        className="w-full h-12 mb-1 mr-1 font-bold text-blue-500 uppercase transition-all duration-150 ease-linear bg-transparent border border-blue-500 border-solid rounded-full outline-none hover:bg-blue-500 hover:text-white active:bg-blue-600 focus:outline-none"
+        className="button-secondary w-full h-12 mb-1 mr-1 font-bold text-blue-500 uppercase transition-all duration-150 ease-linear bg-transparent border border-blue-500 border-solid rounded-full outline-none hover:bg-blue-500 hover:text-white active:bg-blue-600 focus:outline-none"
         type="submit"
       >
         ثبت نام
