e537b106f611dbb4c91116fca05e570be10e0ffc
AmirBahador Bahadori <amirbahador.pv@gmail.com>
2021-11-10 08:03:02 +0000

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 
-
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
@@ -15,7 +14,7 @@ import { createHttpLink } from "apollo-link-http";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "https://api.g5.stagement.ir//graphql",
+  uri: "https://api.g5.stagement.ir/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
